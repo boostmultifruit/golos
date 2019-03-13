9cf4f19579f1a10613aaee72596213f2ab7555b3
maslenitsa93 <maslenitsa93@gmail.com>
2019-03-13 11:24:20 +0300

    Add unit tests on worker_techspec_operation validate #1179

--- tests/tests/worker_techspec_tests.cpp
+++ tests/tests/worker_techspec_tests.cpp
@@ -56,4 +56,58 @@ BOOST_AUTO_TEST_CASE(worker_authorities) {
     }
 }
 
+BOOST_AUTO_TEST_CASE(worker_techspec_validate) {
+    BOOST_TEST_MESSAGE("Testing: worker_techspec_validate");
+
+    BOOST_TEST_MESSAGE("-- Normal case");
+
+    worker_techspec_operation op;
+    op.author = "bob";
+    op.permlink = "techspec-permlink";
+    op.worker_proposal_author = "alice";
+    op.worker_proposal_permlink = "proposal-permlink";
+    op.specification_cost = ASSET_GOLOS(6000);
+    op.development_cost = ASSET_GOLOS(60000);
+    op.payments_interval = 60*60*24;
+    op.payments_count = 2;
+    CHECK_OP_VALID(op);
+
+    BOOST_TEST_MESSAGE("-- Incorrect author or permlink case");
+
+    CHECK_PARAM_INVALID(op, author, "");
+    CHECK_PARAM_INVALID(op, permlink, std::string(STEEMIT_MAX_PERMLINK_LENGTH+1, ' '));
+    CHECK_PARAM_INVALID(op, worker_proposal_author, "");
+    CHECK_PARAM_INVALID(op, worker_proposal_permlink, std::string(STEEMIT_MAX_PERMLINK_LENGTH+1, ' '));
+
+    BOOST_TEST_MESSAGE("-- Non-GOLOS cost case");
+
+    CHECK_PARAM_INVALID(op, specification_cost, ASSET_GBG(6000));
+    CHECK_PARAM_INVALID(op, specification_cost, ASSET_GESTS(6000));
+    CHECK_PARAM_INVALID(op, development_cost, ASSET_GBG(60000));
+    CHECK_PARAM_INVALID(op, development_cost, ASSET_GESTS(60000));
+
+    BOOST_TEST_MESSAGE("-- Negative cost case");
+
+    CHECK_PARAM_INVALID(op, specification_cost, ASSET_GOLOS(-1));
+    CHECK_PARAM_INVALID(op, development_cost, ASSET_GOLOS(-1));
+
+    BOOST_TEST_MESSAGE("-- Zero payments count case");
+
+    CHECK_PARAM_INVALID(op, payments_count, 0);
+
+    BOOST_TEST_MESSAGE("-- Too low payments interval case");
+
+    CHECK_PARAM_INVALID(op, payments_interval, 60*60*24 - 1);
+
+    BOOST_TEST_MESSAGE("-- Single payment with too big interval case");
+
+    op.payments_count = 1;
+    CHECK_PARAM_INVALID(op, payments_interval, 60*60*24 + 1);
+
+    BOOST_TEST_MESSAGE("-- Single payment with normal interval case");
+
+    op.payments_count = 1;
+    CHECK_PARAM_VALID(op, payments_interval, 60*60*24);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
