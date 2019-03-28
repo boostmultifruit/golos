b54d30b12e49af758f9515f755b50bba028e0553
maslenitsa93 <maslenitsa93@gmail.com>
2019-03-28 21:15:44 +0300

    Clear worker payment approves #1169

--- libraries/chain/database_worker_objects.cpp
+++ libraries/chain/database_worker_objects.cpp
@@ -102,6 +102,8 @@ namespace golos { namespace chain {
                     gpo.worker_consumption_per_day -= calculate_worker_techspec_consumption_per_day(*wto_itr);
                 });
 
+                clear_worker_payment_approves(*wto_itr);
+
                 modify(*wto_itr, [&](worker_techspec_object& wto) {
                     wto.finished_payments_count++;
                     wto.next_cashout_time = time_point_sec::maximum();
@@ -149,6 +151,49 @@ namespace golos { namespace chain {
         }
 
         clear_worker_approves<worker_techspec_approve_index, by_techspec_approver>(*this, wto.post);
+    }
+
+    void database::clear_worker_payment_approves(const worker_techspec_object& wto) {
+        if (!_clear_old_worker_approves) {
+            return;
+        }
+
+        clear_worker_approves<worker_result_approve_index, by_result_approver>(*this, wto.worker_result_post);
+    }
+
+    void database::close_worker_techspec(const worker_techspec_object& wto, golos::chain::worker_techspec_state closed_state) {
+        if (wto.state == worker_techspec_state::approved) {
+            const auto& wpo = get_worker_proposal(wto.worker_proposal_post);
+            if (wpo.type == worker_proposal_type::task) {
+                modify(wpo, [&](worker_proposal_object& wpo) {
+                    wpo.state = worker_proposal_state::created;
+                    wpo.approved_techspec_post = comment_id_type();
+                });
+            }
+
+            const auto& gpo = get_dynamic_global_properties();
+            modify(gpo, [&](dynamic_global_property_object& gpo) {
+                gpo.worker_consumption_per_day -= calculate_worker_techspec_consumption_per_day(wto);
+            });
+        }
+
+        const auto& wtao_idx = get_index<worker_techspec_approve_index, by_techspec_approver>();
+        const auto& wrao_idx = get_index<worker_result_approve_index, by_result_approver>();
+        auto wtao_itr = wtao_idx.find(wto.post);
+        auto wrao_itr = wrao_idx.find(wto.worker_result_post);
+        bool has_approves = (wtao_itr != wtao_idx.end() || wrao_itr != wrao_idx.end());
+
+        clear_worker_techspec_approves(wto);
+        clear_worker_payment_approves(wto);
+
+        if (has_approves) {
+            modify(wto, [&](worker_techspec_object& wto) {
+                wto.state = closed_state;
+            });
+            return;
+        }
+
+        remove(wto);
     }
 
     void database::clear_expired_worker_objects() {
