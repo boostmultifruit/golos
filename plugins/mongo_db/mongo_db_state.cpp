be5a633c67283c59b9ce517f25961f6b09021820
maslenitsa93 <maslenitsa93@gmail.com>
2019-06-03 06:25:56 +0300

    Worker fund operation #1107

--- plugins/mongo_db/mongo_db_state.cpp
+++ plugins/mongo_db/mongo_db_state.cpp
@@ -1561,6 +1561,10 @@ namespace mongo_db {
 
     }
 
+    auto state_writer::operator()(const worker_fund_operation& op) -> result_type {
+
+    }
+
     auto state_writer::operator()(const fill_convert_request_operation& op) -> result_type {
         try {
             format_account(op.owner);
--- tests/tests/worker_payment_tests.cpp
+++ tests/tests/worker_payment_tests.cpp
@@ -16,12 +16,21 @@ BOOST_FIXTURE_TEST_SUITE(worker_payment_tests, worker_fixture)
 BOOST_AUTO_TEST_CASE(worker_authorities) {
     BOOST_TEST_MESSAGE("Testing: worker_authorities");
 
-    worker_payment_approve_operation op;
-    op.approver = "cyberfounder";
-    op.worker_techspec_author = "bob";
-    op.worker_techspec_permlink = "bob-techspec";
-    op.state = worker_techspec_approve_state::approve;
-    CHECK_OP_AUTHS(op, account_name_set(), account_name_set(), account_name_set({"cyberfounder"}));
+    {
+        worker_payment_approve_operation op;
+        op.approver = "cyberfounder";
+        op.worker_techspec_author = "bob";
+        op.worker_techspec_permlink = "bob-techspec";
+        op.state = worker_techspec_approve_state::approve;
+        CHECK_OP_AUTHS(op, account_name_set(), account_name_set(), account_name_set({"cyberfounder"}));
+    }
+
+    {
+        worker_fund_operation op;
+        op.sponsor = "alice";
+        op.amount = ASSET_GOLOS(10);
+        CHECK_OP_AUTHS(op, account_name_set(), account_name_set(), account_name_set({"alice"}));
+    }
 }
 
 BOOST_AUTO_TEST_CASE(worker_payment_approve_validate) {
@@ -346,8 +355,8 @@ BOOST_AUTO_TEST_CASE(worker_payment_disapprove) {
     }
 }
 
-BOOST_AUTO_TEST_CASE(worker_fund) {
-    BOOST_TEST_MESSAGE("Testing: worker_fund");
+BOOST_AUTO_TEST_CASE(worker_fund_filling) {
+    BOOST_TEST_MESSAGE("Testing: worker_fund_filling");
 
     generate_block();
 
@@ -360,6 +369,65 @@ BOOST_AUTO_TEST_CASE(worker_fund) {
     const auto& gpo = db->get_dynamic_global_properties();
     BOOST_CHECK_EQUAL(gpo.total_worker_fund_steem, fund.worker_fund());
     BOOST_CHECK_EQUAL(gpo.worker_revenue_per_day, (gpo.total_worker_fund_steem - old_fund) * STEEMIT_BLOCKS_PER_DAY);
+}
+
+BOOST_AUTO_TEST_CASE(worker_fund_validate) {
+    BOOST_TEST_MESSAGE("Testing: worker_fund_validate");
+
+    BOOST_TEST_MESSAGE("-- Normal case");
+
+    worker_fund_operation op;
+    op.sponsor = "alice";
+    op.amount = ASSET_GOLOS(10);
+    CHECK_OP_VALID(op);
+
+    BOOST_TEST_MESSAGE("-- Incorrect account case");
+
+    CHECK_PARAM_INVALID(op, sponsor, "");
+
+    BOOST_TEST_MESSAGE("-- Invalid symbol cases");
+
+    CHECK_PARAM_INVALID(op, amount, ASSET_GBG(10));
+    CHECK_PARAM_INVALID(op, amount, ASSET_GESTS(10));
+
+    BOOST_TEST_MESSAGE("-- Invalid amount cases");
+
+    CHECK_PARAM_INVALID(op, amount, ASSET_GOLOS(0));
+    CHECK_PARAM_INVALID(op, amount, ASSET_GOLOS(-10));
+}
+
+BOOST_AUTO_TEST_CASE(worker_fund_apply) {
+    BOOST_TEST_MESSAGE("Testing: worker_fund_apply");
+
+    signed_transaction tx;
+
+    ACTORS((alice))
+    fund("alice", 15000);
+
+    BOOST_TEST_MESSAGE("-- Normal funding");
+
+    const auto init_balance = db->get_balance("alice", STEEM_SYMBOL);
+    const auto init_fund = db->get_dynamic_global_properties().total_worker_fund_steem;
+
+    generate_block();
+    const auto block_add = db->get_dynamic_global_properties().total_worker_fund_steem - init_fund;
+
+    worker_fund_operation op;
+    op.sponsor = "alice";
+    op.amount = ASSET_GOLOS(10);
+    BOOST_CHECK_NO_THROW(push_tx_with_ops(tx, alice_private_key, op));
+    generate_block();
+
+    BOOST_CHECK_EQUAL(db->get_balance("alice", STEEM_SYMBOL), init_balance - op.amount);
+    BOOST_CHECK_EQUAL(db->get_dynamic_global_properties().total_worker_fund_steem, init_fund + block_add*2 + op.amount);
+
+    BOOST_TEST_MESSAGE("-- Trying fund more than have");
+
+    op.amount = db->get_balance("alice", STEEM_SYMBOL);
+    op.amount += ASSET_GOLOS(1);
+    GOLOS_CHECK_ERROR_PROPS(push_tx_with_ops(tx, alice_private_key, op),
+        CHECK_ERROR(tx_invalid_operation, 0,
+            CHECK_ERROR(insufficient_funds, "alice", "fund", op.amount)));
 }
 
 BOOST_AUTO_TEST_CASE(worker_cashout) {
